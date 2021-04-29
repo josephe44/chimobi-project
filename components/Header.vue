d6de55f5d737e96e8e7c23492664fa001c180ecb
audeypx <audreypx100@gmail.com>
2021-04-29 16:18:02 +0100

    Added Header Component

---
+++ components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="bg-gray-50 py-4 px-8 flex justify-between items-center">
+    <div class="flex items-center">
+      <!-- <img src="" width="50" alt="Logo" class="" /> -->
+      <div class="w-20 h-10 bg-gray-400"></div>
+    </div>
+
+    <div class="font-medium">
+      <a href="#" class="py-2 px-4 text-gray-600">Home</a>
+      <a href="#" class="py-2 px-4 text-gray-600">About Us</a>
+      <a href="#" class="py-2 px-4 text-gray-600">Services</a>
+      <a href="#" class="py-2 px-4 text-gray-600">Testimonials</a>
+      <a href="#" class="py-2 px-4 text-gray-600">Payment</a>
+    </div>
+  </div>
+</template>
