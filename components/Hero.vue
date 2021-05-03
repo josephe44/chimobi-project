4ae64a634576d4c530cb2a3444375379266b070a
audeypx <audreypx100@gmail.com>
2021-05-03 20:58:11 +0100

    added responsivness

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="bg-hero-pattern px-8 flex">
-    <div class="m-auto text-gray-700 w-full">
-      <h1 class="font-extrabold text-center text-5xl">
-        The outsourced marketing unagency you can trust
+  <div class="bg-hero-pattern md:px-8 flex">
+    <div class="m-auto text-gray-700 md:w-4/5">
+      <h1
+        class="md:w-11/12 m-auto w-full font-extrabold text-center md:text-7xl text-5xl capitalize"
+      >
+        re-invent your career with our online course
       </h1>
-      <p class="text-center font-light py-4 text-2xl">
+      <p class="md:w-3/5 w-11/12 text-center font-light py-4 text-base m-auto">
         Are you looking for branding, leads and conversions, generating traffic
         for your local business, sales, app installs, organic traffic, coverage
         and visibility or do you want to learn a high paying skills that will
@@ -20,8 +22,18 @@
   background-image: url(https://res.cloudinary.com/softnexus/image/upload/v1596465429/assets/images/nav/hero_section_img_p19gtf.svg);
   background-position-x: 50%;
   width: 100%;
-  height: 680px;
+  height: 500px;
   background-size: cover;
   background-repeat: no-repeat;
+}
+@media (min-width: 768px) {
+  .bg-hero-pattern {
+    background-image: url(https://res.cloudinary.com/softnexus/image/upload/v1596465429/assets/images/nav/hero_section_img_p19gtf.svg);
+    background-position-x: 50%;
+    width: 100%;
+    height: 680px;
+    background-size: cover;
+    background-repeat: no-repeat;
+  }
 }
 </style>
